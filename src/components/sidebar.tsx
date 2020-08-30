03594833dcba73e60f68c864bf13f3c7560a5850
akeno <stepfootprint@gmail.com>
2020-08-31 01:51:38 +0900

    https対応

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import houkago from '../images/banner/houkago.jpg';
 
 const useStyles = makeStyles({});
 
@@ -11,7 +11,7 @@ export default function SideBar() {
   return (
     <Box m={1}>
       <Box>
-        <a href='http://www.hook-net.jp/houkago/' target='_blank'><img src='http://www.hook-net.jp/houkago/banner/bnr300x250-B.jpg' alt='『放課後シンデレラ』を応援しています！' border='0' /></a>
+        <a href='http://www.hook-net.jp/houkago/' target='_blank'><img src={houkago} alt='『放課後シンデレラ』を応援しています！' border='0' /></a>
       </Box>
     </Box>
   )
